a85038b2069e10ac6bc340e74f9f24a29693d045
Timore <tber.insa@gmail.com>
2018-04-21 19:56:33 +0200

    Test Environnement juste commenté

--- tests/testsEnvironnement.h
+++ tests/testsEnvironnement.h
@@ -1,13 +1,37 @@
 #ifndef TESTSENVIRONNEMENT_H
 #define TESTSENVIRONNEMENT_H
 
+#include <iostream>
+#include <string>
+#include <cppunit/TestCase.h>
+#include <cppunit/TestFixture.h>
+#include <cppunit/ui/text/TextTestRunner.h>
+#include <cppunit/extensions/HelperMacros.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/XmlOutputter.h>
+
+#include "../include/Environnement.h"
+
 
 class testsEnvironnement
 {
+
+  CPPUNIT_TEST_SUITE(testsEnvironnement);
+  CPPUNIT_TEST(testcalculerPos);
+  CPPUNIT_TEST(testcolision);
+  CPPUNIT_TEST_SUITE_END();
+
     public:
-        testsEnvironnement();
-        virtual ~testsEnvironnement();
+      void setup(void);
+      void tearDown(void);
     protected:
+      void testcalculerPos(void);
+      void testcolision(void);
     private:
 };
 
